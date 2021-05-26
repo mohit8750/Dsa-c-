8ed469f104a345cd45bc008a46b43ff2572b0f59
mohit8750 <80882834+mohit8750@users.noreply.github.com>
2021-05-26 12:39:44 +0530

    Update removeDup.cpp

--- Dsa_question_recursion/removeDup.cpp
+++ Dsa_question_recursion/removeDup.cpp
@@ -1,19 +1,24 @@
-#include<iostream>
+
+//You are given a string ‘str’ of size ‘N’. Your task is to remove consecutive duplicates from this string recursively.
+#include <iostream>
 using namespace std;
-string xtoend(string s){
-    if(s.length()==0){
-        return "";
+
+string removeDuplicate(string &s)
+{
+    if(s.length()==0) return "";
+    char first = s[0];
+    string sub = s.substr(1);
+    string ans = removeDuplicate(sub);
+    if(first==ans[0]){
+        return ans;
     }
-    char first=s[0];
-    string ans=xtoend(s.substr(1));
-    if(first=='x'){
-        return (ans+first);
-    }
-    return (first+ans);
+ 
+        return (first+ans);
+    
 }
 int main(){
     string s;
     cin>>s;
-    cout<<xtoend(s);
+    cout<<removeDuplicate(s);
     return 0;
 }
